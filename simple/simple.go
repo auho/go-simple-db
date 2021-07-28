428fbce4f67188256ccc081219caf166d56591b3
auho <auho@163.com>
2021-07-28 14:20:49 +0800

    dev

--- simple/simple.go
+++ simple/simple.go
@@ -210,7 +210,7 @@ func (e *Engine) GenerateBulkInsertPrepareQuery(tableName string, fields []strin
 }
 
 func (e *Engine) GenerateUpdatePrepareQuery(tableName string, keyName string, unSavedRow map[string]interface{}) (query string, fields []string, err error) {
-	if len(unSavedRow) <= 1 {
+	if len(unSavedRow) <= 0 {
 		return "", nil, errors.New("row is error")
 	}
 
