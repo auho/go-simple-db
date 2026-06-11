a3a59ef2bec9b14a269c42f06668854f7202ebd9
auho <auho@163.com>
2026-06-11 14:22:32 +0800

    refactor: update factory imports for new driver directory structure
    - Import mysql gorm driver from driver/mysql/gorm
    - Import clickhouse gorm driver from driver/clickhouse/gorm
    - Use import aliases to avoid package name conflicts

--- driver.go
+++ driver.go
@@ -1,24 +1,25 @@
 package go_simple_db
 
 import (
-	"github.com/auho/go-simple-db/v2/driver/clickhouse"
+	"github.com/auho/go-simple-db/v2/driver/clickhouse/gorm"
+	mysqlgorm "github.com/auho/go-simple-db/v2/driver/mysql/gorm"
+
 	"github.com/auho/go-simple-db/v2/driver/driver"
-	"github.com/auho/go-simple-db/v2/driver/mysql"
-	"gorm.io/gorm"
+	gormlib "gorm.io/gorm"
 )
 
 // NewMySQL
-// new mysql
-func NewMySQL(dsn string, opts ...gorm.Option) (*SimpleDB, error) {
+// new mysql with gorm driver
+func NewMySQL(dsn string, opts ...gormlib.Option) (*SimpleDB, error) {
 	return NewSimple(func() (driver.Driver, error) {
-		return mysql.NewMySQL(dsn, opts...)
+		return mysqlgorm.NewMySQL(dsn, opts...)
 	})
 }
 
 // NewClickHouse
-// new clickhouse
-func NewClickHouse(dsn string, opts ...gorm.Option) (*SimpleDB, error) {
+// new clickhouse with gorm driver
+func NewClickHouse(dsn string, opts ...gormlib.Option) (*SimpleDB, error) {
 	return NewSimple(func() (driver.Driver, error) {
-		return clickhouse.NewClickHouse(dsn, opts...)
+		return gorm.NewClickHouse(dsn, opts...)
 	})
 }
